9072e08490dc03c49dc43e8265012a7123337292
cunhamuril <murilo.sant@hotmail.com>
2020-03-24 16:03:34 -0300

    :pencil: (index) concepts

--- backend/index.js
+++ backend/index.js
@@ -2,11 +2,38 @@ const express = require("express");
 
 const app = express();
 
-app.get("/", (req, res) => {
+/**
+ * Rota / Recurso
+ */
+
+/**
+ * Métodos HTTP:
+ *
+ * GET: Buscar/listar uma informação do back-end;
+ * POST: Criar uma informação no back-end;
+ * PUT: Alterar uma informação no back-end;
+ * DELETE: Deletar uma informação no back-end.
+ */
+
+/**
+ * Tipos de parâmetros:
+ *
+ * Query Params: Parâmetros nomeados enviados na rota após "?" (Filtros, paginação);
+ * Route Params: Parâmetros utilizados para identificar recursos;
+ * Request Body: Corpo da requisição, utilizado para criar ou alterar recursos.
+ */
+
+/**
+ * SQL: MySQL, SQLite, PostgreSQL, Oracle, Microsoft SQL Server;
+ * NoSQL: MongoDB, CouchDB, etc.
+ */
+
+app.get("/users", (req, res) => {
   return res.json({
     evento: "Semana Omnistack 11.0",
     aluno: "Murilo Santos Cunha"
   });
 });
 
-app.listen(3333);
+const port = 3333;
+app.listen(port, () => console.log(`Backend running on port ${port} :)`));
