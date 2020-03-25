8dc22329b2a46e5e3519edccaf175b128eeb045d
cunhamuril <murilo.sant@hotmail.com>
2020-03-25 00:36:06 -0300

    :sparkles: (index) Add cors

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,9 +1,19 @@
 const express = require("express");
+const cors = require("cors");
 
 const routes = require("./routes");
 
 const app = express();
 
+/**
+ * Se fosse para produção e não fosse uma API pública, o cors seria assim:
+ *
+ * app.use(cors({
+ *  origin: 'http://meu-app.com'
+ * }));
+ */
+
+app.use(cors());
 app.use(express.json()); // Utilizar requisições no formato JSON
 app.use(routes);
 
